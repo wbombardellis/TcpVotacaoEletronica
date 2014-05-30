0772aac0e860d576b980ad7eaba8e991dacdcb59
wbombardellis <wbombardellis@gmail.com>
2014-05-29 22:03:49 -0300

    Mais Correções Código e Diagrama. Inicio implementação Main implementada

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,9 +1,14 @@
 package controller;
 
+import view.View;
+import model.Inicializador;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Inicializador.incializaDao();
+		
+		new View().imprimeMenu();
 	}
 
 }
--- src/controller/VotarController.java
+++ src/controller/VotarController.java
@@ -1,20 +1,43 @@
 package controller;
 
+import model.dao.VotacaoDao;
 import model.entity.Votacao;
 import model.entity.Voto;
 import model.entity.Membro;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class VotarController {
 
 	public static void insereVoto(Votacao votacao, Voto voto) {
-
+		//Copia os votos da votação enviada
+		ArrayList<Voto> novosVotos = new ArrayList<>(votacao.getVotos());
+		//Adiciona novo voto
+		novosVotos.add(voto);
+		
+		//Atualiza a votação no DAO com uma nova instância. Faz isso pois Votacao é imutável
+		VotacaoDao.getInstance().update(votacao.getId(), new Votacao(votacao, novosVotos));
 	}
 
 	public static void removeVoto(Votacao votacao, Voto voto) {
-
+		//Copia os votos da votação enviada
+		ArrayList<Voto> novosVotos = new ArrayList<>(votacao.getVotos());
+		//Remove tal voto
+		novosVotos.remove(voto);
+		
+		//Atualiza a votação no DAO com uma nova instância. Faz isso pois Votacao é imutável
+		VotacaoDao.getInstance().update(votacao.getId(), new Votacao(votacao, novosVotos));
 	}
 
 	public static Voto getVotoByAutor(Votacao votacao, Membro autor) {
+		List<Voto> votos = votacao.getVotos();
+		
+		for(Voto voto : votos){
+			if (voto.getAutor().equals(autor)){
+				return voto;
+			}
+		}
 		return null;
 	}
 
