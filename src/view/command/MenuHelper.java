9ef1bfdd31df1f038ed7183464b861bddda58a85
wbombardellis <wbombardellis@gmail.com>
2014-06-01 13:06:37 -0300

    MenuHelper Implementação continuação

--- src/view/command/MenuHelper.java
+++ src/view/command/MenuHelper.java
@@ -3,6 +3,7 @@ package view.command;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class MenuHelper {
@@ -11,18 +12,58 @@ public abstract class MenuHelper {
 
 	public static <T extends Imprimivel> T leOpcaoMenu(List<T> opcoes) throws IOException {
 		
-		TextManager txtManager = new TextManager(SaidaHelper.nomeRecursos);
+		imprimeOpcoes(opcoes);
 		
-		//Imprime op��es
+		SaidaHelper.imprimeLinhaFromResources("mensagem.menu.opcao.escolher");
+		
+		return leOpcao(opcoes);
+	}
+	
+	private static <T extends Imprimivel> T getOpcaoByCodigo(List<T> opcoes, String codigo) {
+		for (T opcao : opcoes){
+			if (opcao.getCodigoTela().equals(codigo))
+				return opcao;
+		}
+		return null;
+	}
+
+	public static <T extends Imprimivel> List<T> leOpcoesMenu(List<T> opcoes) throws IOException {
+		
+		ArrayList<T> opcoesCopia = new ArrayList<>(opcoes);
+		ArrayList<T> opcoesEscolhidas = new ArrayList<>(opcoes);
+		T opcao;
+		Boolean fimLeitura;
+		do{
+			imprimeOpcoes(opcoesCopia);
+			SaidaHelper.imprimeLinhaFromResources("mensagem.menu.opcao.escolher");
+		
+			opcao = leOpcao(opcoesCopia);
+			if (opcao == null){
+				fimLeitura = true;
+				opcoesEscolhidas = null;
+			}else{
+				opcoesEscolhidas.add(opcao);
+				opcoesCopia.remove(opcao);
+				//Ler mais opções? @ TODO
+				fimLeitura = false;
+			}
+		}while(!fimLeitura);
+		
+		return opcoesEscolhidas;
+	}
+
+	public static <T extends Imprimivel> void imprimeOpcoes(List<T> opcoes)
+	{
+		TextManager txtManager = new TextManager(SaidaHelper.nomeRecursos);
+		//Imprime op��es
 		for (T opcao : opcoes)
 		{
 			SaidaHelper.imprimeLinha(opcao.getCodigoTela() + txtManager.getText("simbolo.menu.separadorCodigoDescricao") + opcao.getDescricaoTela());
 		}
 		SaidaHelper.imprimeLinha(codigoCancelar + " - " + txtManager.getText("simbolo.menu.opcao.cancelar"));
-		
-		SaidaHelper.imprimeLinhaFromResources("mensagem.menu.opcao.escolher");
-		
-		//Lê opção
+	}
+	
+	public static <T extends Imprimivel> T leOpcao(List<T> opcoes) throws IOException{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		Boolean entradaInvalida;
 		String escolha;
@@ -40,22 +81,5 @@ public abstract class MenuHelper {
 			return null;
 		else
 			return getOpcaoByCodigo(opcoes, escolha);
-	}
-	
-	private static <T extends Imprimivel> T getOpcaoByCodigo(List<T> opcoes, String codigo) {
-		for (T opcao : opcoes){
-			if (opcao.getCodigoTela().equals(codigo))
-				return opcao;
-		}
-		return null;
-	}
-
-	public static <T extends Imprimivel> List<T> leOpcoesMenu(List<T> opcoes) {
-		return null;
-	}
-
-	public static <T extends Imprimivel> void imprimeOpcoes(List<T> opcoes)
-	{
-		
 	}
 }
